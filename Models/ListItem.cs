b57148404da2f026eb216e7c8fd6259114fe85ef
Dennis Kim <dkim334@my.bcit.ca>
2023-07-12 13:32:25 -0700

    added gmaps api key as env. added ListItem model

--- Models/ListItem.cs
+++ Models/ListItem.cs
@@ -2,11 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GoogleMaps;
+
 
 namespace Fooli.Models
 {
-    public class ListItem
-    {
-        
-    }
+  public class ListItem
+  {
+    public string LocationName { get; set; }
+    public double? LocationStarRating { get; set; }
+    public int? LocationPriceLevel { get; set; }
+    public string LocationAddress { get; set; }
+    public LatLng LocationLatLng { get; set; }
+    public string LocationImage { get; set; }
+    public string Notes { get; set; }
+    public DateTime DateAdded { get; set; }
+  }
 }
--- Program.cs
+++ Program.cs
@@ -3,14 +3,24 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Load environment variables from the .env file
+var configuration = new ConfigurationBuilder()
+    .AddEnvironmentVariables()
+    .AddJsonFile(".env", optional: true, reloadOnChange: true)
+    .Build();
+
+// Get the Google Maps API key from the configuration
+var googleMapsApiKey = configuration["GoogleMapsApiKey"];
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  app.UseExceptionHandler("/Home/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
