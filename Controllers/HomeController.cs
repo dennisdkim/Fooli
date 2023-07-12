50f3f400d7b89fd0d74ffdcc622810ad6822bf51
Dennis Kim <dkim334@my.bcit.ca>
2023-07-12 14:37:11 -0700

    added map to index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,26 +6,36 @@ namespace Fooli.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+  private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+  public HomeController(ILogger<HomeController> logger)
+  {
+    _logger = logger;
+  }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+  public IActionResult Index()
+  {
+    var googleMapsApiKey = Environment.GetEnvironmentVariable("GoogleMapsApiKey");
 
-    public IActionResult Privacy()
+    // Create an instance of the List model and populate other properties
+    var list = new List
     {
-        return View();
-    }
+    };
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+    // Pass the model and the API key to the view
+    ViewBag.GoogleMapsApiKey = googleMapsApiKey;
+    return View(list);
+  }
+
+  public IActionResult Privacy()
+  {
+    return View();
+  }
+
+
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+  public IActionResult Error()
+  {
+    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+  }
 }
